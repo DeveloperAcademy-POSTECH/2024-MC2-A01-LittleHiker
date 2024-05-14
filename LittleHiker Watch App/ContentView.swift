e8523b305fb2bab7822435cac8aefa632a75c0f0
rokdam <travel_dob@naver.com>
2024-05-14 12:33:26 +0900

    .gitignore 추가, userdata 삭제

---
+++ LittleHiker Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleHiker Watch App
+//
+//  Created by Lyosha's MacBook   on 5/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
