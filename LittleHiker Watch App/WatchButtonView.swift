f6c34d6b41f5ad8fa8ebd04282183d8ee3c7a429
norang <norang1221@gmail.com>
2024-05-16 16:18:00 +0900

    Refactor: [feature/WatchButtonView] #태스크번호없음 버튼뷰 코드 리팩토링

--- LittleHiker Watch App/WatchButtonView.swift
+++ LittleHiker Watch App/WatchButtonView.swift
@@ -16,7 +16,7 @@ struct WatchButtonView: View {
             HStack {
                 Spacer()
                 //State or Binding 변수에 따라 등산중/ 정상/ 하산중 구현 예정
-                //?? 이거 위치 고정이 안되는듯 나중에 기본 컴포넌트로 보여주는 방법이 따로 있을까??
+                //이 부분 아마 네비게이션으로 될듯?
                 Text("\(hikingStatus)")
                     .foregroundStyle(Color.blue)
             }
@@ -26,62 +26,18 @@ struct WatchButtonView: View {
                 VStack {
                     HStack {
                         //종료버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 44)
-                                .foregroundColor(.red)
-                                .opacity(0.25)
-                                .overlay {
-                                    Image(systemName: "xmark")
-                                        .foregroundStyle(Color.red)
-                                }
-                            Text("종료")
-                                .font(.system(size: 12))
-                        }
+                        StopButton(height: 44)
                         
                         //일시정지버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 44)
-                                .foregroundColor(.yellow)
-                                .opacity(0.25)
-                                .overlay {
-                                    Image(systemName: "pause.fill")
-                                        .foregroundStyle(Color.yellow)
-                                }
-                            Text("일시정지")
-                                .font(.system(size: 12))
-                        }
+                        PauseButton(height: 44)
+                        //재개버튼
+//                      RestartButton(height: 44)
                     }
-                    
                     HStack {
                         //정상버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 44)
-                                .foregroundColor(.green)
-                                .opacity(0.25)
-                                .overlay {
-                                    Image(systemName: "mountain.2.fill")
-                                        .foregroundStyle(Color.green)
-                                }
-                            Text("정상")
-                                .font(.system(size: 12))
-                        }
-                        
+                        PeakButton(height: 44)
                         //하산버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 44)
-                                .foregroundColor(.white)
-                                .opacity(0.12)
-                                .overlay {
-                                    Image(systemName: "arrow.down.right")
-                                        .foregroundStyle(Color.white)
-                                }
-                            Text("하산")
-                                .font(.system(size: 12))
-                        }
+                        DescendButton(height: 44)
                     }
                     
                 }
@@ -90,32 +46,9 @@ struct WatchButtonView: View {
                 VStack {
                     HStack {
                         //종료버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 56)
-                                .foregroundColor(.red)
-                                .opacity(0.25)
-                                .overlay {
-                                    Image(systemName: "xmark")
-                                        .foregroundStyle(Color.red)
-                                }
-                            Text("종료")
-                                .font(.system(size: 12))
-                        }
-                        
+                        StopButton(height: 56)
                         //하산버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 56)
-                                .foregroundColor(.white)
-                                .opacity(0.12)
-                                .overlay {
-                                    Image(systemName: "arrow.down.right")
-                                        .foregroundStyle(Color.white)
-                                }
-                            Text("하산")
-                                .font(.system(size: 12))
-                        }
+                        DescendButton(height: 56)
                     }
                     Spacer()
                 }
@@ -124,32 +57,12 @@ struct WatchButtonView: View {
                 VStack {
                     HStack {
                         //종료버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 56)
-                                .foregroundColor(.red)
-                                .opacity(0.25)
-                                .overlay {
-                                    Image(systemName: "xmark")
-                                        .foregroundStyle(Color.red)
-                                }
-                            Text("종료")
-                                .font(.system(size: 12))
-                        }
+                        StopButton(height: 56)
                         
                         //일시정지버튼
-                        VStack {
-                            RoundedRectangle(cornerRadius: 28)
-                                .frame(width: 68, height: 56)
-                                .foregroundColor(.yellow)
-                                .opacity(0.25)
-                                .overlay {
-                                    Image(systemName: "pause.fill")
-                                        .foregroundStyle(Color.yellow)
-                                }
-                            Text("일시정지")
-                                .font(.system(size: 12))
-                        }
+                        PauseButton(height: 56)
+                        //재개버튼
+//                      RestartButton(height: 56)
                     }
                 }
                 .padding()
@@ -158,6 +71,113 @@ struct WatchButtonView: View {
         }
     }
 }
+
+
+//종료버튼
+struct StopButton: View {
+    var height: CGFloat
+
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 28)
+                .frame(width: 68, height: height)
+                .foregroundColor(.red)
+                .opacity(0.25)
+                .overlay {
+                    Image(systemName: "xmark")
+                        .foregroundStyle(Color.red)
+                        .fontWeight(.bold)
+                }
+            Text("종료")
+                .font(.system(size: 12))
+        }
+    }
+}
+
+//일시정지버튼
+struct PauseButton: View {
+    var height: CGFloat
+
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 28)
+                .frame(width: 68, height: height)
+                .foregroundColor(.yellow)
+                .opacity(0.25)
+                .overlay {
+                    Image(systemName: "arrow.circlepath")
+                        .foregroundStyle(Color.yellow)
+                        .fontWeight(.bold)
+                }
+            Text("재개")
+                .font(.system(size: 12))
+        }
+    }
+}
+
+//재개버튼
+struct RestartButton: View {
+    var height: CGFloat
+
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 28)
+                .frame(width: 68, height: height)
+                .foregroundColor(.yellow)
+                .opacity(0.25)
+                .overlay {
+                    Image(systemName: "arrow.circlepath")
+                        .foregroundStyle(Color.yellow)
+                        .fontWeight(.bold)
+                }
+            Text("재개")
+                .font(.system(size: 12))
+        }
+    }
+}
+
+//정상버튼
+struct PeakButton: View {
+    var height: CGFloat
+    
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 28)
+                .frame(width: 68, height: height)
+                .foregroundColor(.green)
+                .opacity(0.25)
+                .overlay {
+                    Image(systemName: "mountain.2.fill")
+                        .foregroundStyle(Color.green)
+                        .fontWeight(.bold)
+                }
+            Text("정상")
+                .font(.system(size: 12))
+        }
+    }
+}
+
+//하산버튼
+struct DescendButton: View {
+    var height: CGFloat
+
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 28)
+                .frame(width: 68, height: height)
+                .foregroundColor(.white)
+                .opacity(0.12)
+                .overlay {
+                    Image(systemName: "arrow.down.right")
+                        .foregroundStyle(Color.white)
+                        .fontWeight(.bold)
+                }
+            Text("하산")
+                .font(.system(size: 12))
+        }
+    }
+}
+
 
 #Preview {
     WatchButtonView()
