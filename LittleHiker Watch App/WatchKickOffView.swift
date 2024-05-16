341d6178e86b725b3e536f3d446e62a64161d42a
박서현 <parkseohyun@10-141-55-249.0200.pos.kr.iosda.org>
2024-05-16 21:26:05 +0900

    Comment: [feature/WatchKickOffView] - 주석변경 - 스타일 변경

--- LittleHiker Watch App/WatchKickOffView.swift
+++ LittleHiker Watch App/WatchKickOffView.swift
@@ -18,7 +18,7 @@ struct WatchKickOffView: View {
                 .overlay {
                     Text("시작")
                         .font(.system(size: 24))
-                        .fontWeight(.bold)
+                        .fontWeight(.semibold)
                         .foregroundStyle(.black)
                 }
                 .shadow(color: .green.opacity(0.5), radius: 12)
@@ -56,8 +56,10 @@ struct WatchKickOffView: View {
         //3,2,1 화면
         //        CircularProgressView()
         //            .frame(width: 96, height: 96)
+        
     }
-    //준비화면 func
+    
+    //준비화면의 progress를 0부터 1까지 채웁니다.
     //    func decreaseProgress() {
     //        if progress == 0 {
     //            progress += 1
@@ -66,15 +68,17 @@ struct WatchKickOffView: View {
     //        }
     //    }
 }
-//
+//progress와 count가 포함된 구조체
 //struct CircularProgressView: View {
 //    @State var progress: Double = 1
 //    @State var count: Int = 3
 //    
 //    var body: some View {
 //        ZStack {
+//배경에 깔려있는 빈 프로그레스
 //            Circle()
 //                .stroke(Color.green.opacity(0.5), lineWidth: 8)
+//채워지는 프로그레스
 //            Circle()
 //                .trim(from: 0, to: progress)
 //                .stroke(Color.green,
@@ -94,7 +98,7 @@ struct WatchKickOffView: View {
 //        }
 //    }
 //    
-//3,2,1 화면 func
+//1초가 지날 때마다 -.33값이 되는 progress입니다.
 //    func decreaseProgress() {
 //        if progress > 0.1 {
 //            if progress != 1 {
