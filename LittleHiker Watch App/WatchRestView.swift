3eaa91ad5953fbc695b5206ea2687725fd0c0321
sungkug_apple_developer_ac <sunglee@Kkugy-MackBook-Pro.local>
2024-05-16 23:30:31 +0900

    Style: [feature/WatchMainView] #20 코드 수정

---
+++ LittleHiker Watch App/WatchRestView.swift
@@ -0,0 +1,18 @@
+//
+//  WatchRestView.swift
+//  LittleHiker Watch App
+//
+//  Created by sungkug_apple_developer_ac on 5/16/24.
+//
+
+import SwiftUI
+
+struct WatchRestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WatchRestView()
+}
