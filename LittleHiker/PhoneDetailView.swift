6841c785097444af7db5dd52676dbc51cbb8e79b
Hyungeol Lee <ekfrhkquf12@naver.com>
2024-05-19 23:34:43 +0900

    Style: [feature/PhoneDetailView] #1 컴포넌트 간격 조정 및 텍스트 강조 효과 추가

--- LittleHiker/PhoneDetailView.swift
+++ LittleHiker/PhoneDetailView.swift
@@ -11,17 +11,26 @@ struct PhoneDetailView: View {
     var body: some View {
         ScrollView {
             HStack{
-                Text("등산 고도")
+                Text("산행 시간")
+                    .bold()
+                    .frame(width: 150)
                 Text("04:08:22")
+                    .frame(width: 250, alignment: .leading)
             }
             Divider()
             HStack{
                 Text("등반 고도")
+                    .bold()
+                    .frame(width: 150)
                 Text("223km ~ 786km")
+                    .frame(width: 250, alignment:
+                            .leading)
             }
             Divider()
             HStack{
                 Text("평균 속도")
+                    .bold()
+                    .frame(width: 150)
                 VStack{
                     HStack{
                         Text("등산평균속도")
@@ -31,12 +40,15 @@ struct PhoneDetailView: View {
                         Text("하산평균속도")
                         Text("4km/h")
                     }
-                }
+                }.frame(width: 250, alignment: .leading)
             }
             Divider()
             HStack{
                 Text("평균충격량")
+                    .bold()
+                    .frame(width: 150)
                 Text( "XXXXXX ")
+                    .frame(width: 250, alignment: .leading)
             }
         }
         .navigationTitle("2024/05/01")
