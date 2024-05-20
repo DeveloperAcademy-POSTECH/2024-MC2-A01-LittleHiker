1fb1b7e6e2e74a45214418a9fe3058ea4cbd8e22
박서현 <parkseohyun@10-141-55-249.0200.pos.kr.iosda.org>
2024-05-20 15:15:37 +0900

    Design: [feature/WatchKickOffView] - 버튼 크기 조정 - 그린에서 메인 컬러로 변경 - 폰트 사이즈 변경

--- LittleHiker Watch App/WatchKickOffView.swift
+++ LittleHiker Watch App/WatchKickOffView.swift
@@ -12,35 +12,37 @@ struct WatchKickOffView: View {
     @State var progress: Double = 0
     var body: some View {
         //시작화면
-        
         Button {
             viewModel.status = .hiking
         } label: {
             VStack {
                 Circle()
-                    .frame(width: 96, height: 96)
-                    .foregroundColor(.green)
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(.main)
                     .overlay {
                         Text("시작")
-                            .font(.system(size: 24))
+                            .font(.system(size: 32))
                             .fontWeight(.semibold)
                             .foregroundStyle(.black)
                     }
-                    .shadow(color: .green.opacity(0.5), radius: 12)
-                Text("리틀하이커랑 등산하기")
-                    .font(.system(size: 12))
-                    .fontWeight(.medium)
-                    .padding(.top, 12)
+                    .shadow(color: .main.opacity(0.3), radius: 12)
             }
             .padding()
         }
+        .buttonStyle(PlainButtonStyle())
+        
+        Text("리틀하이커랑 등산하기")
+            .font(.system(size: 14))
+            .fontWeight(.medium)
+            .padding(.top, 16)
+        
         
         
         //준비하기 화면
         //        VStack {
         //            Circle()
         //                .trim(from: 0, to: progress)
-        //                .stroke(Color.green,
+        //                .stroke(Color.main,
         //                        style: StrokeStyle(
         //                            lineWidth: 8,
         //                            lineCap: .round))
@@ -61,8 +63,8 @@ struct WatchKickOffView: View {
         
         
         //3,2,1 화면
-        //        CircularProgressView()
-        //            .frame(width: 96, height: 96)
+//                CircularProgressView()
+//                    .frame(width: 96, height: 96)
         
     }
     
@@ -82,13 +84,13 @@ struct WatchKickOffView: View {
 //
 //    var body: some View {
 //        ZStack {
-//배경에 깔려있는 빈 프로그레스
+////배경에 깔려있는 빈 프로그레스
 //            Circle()
-//                .stroke(Color.green.opacity(0.5), lineWidth: 8)
-//채워지는 프로그레스
+//                .stroke(Color.main.opacity(0.5), lineWidth: 8)
+////채워지는 프로그레스
 //            Circle()
 //                .trim(from: 0, to: progress)
-//                .stroke(Color.green,
+//                .stroke(Color.main,
 //                        style: StrokeStyle(
 //                            lineWidth: 8,
 //                            lineCap: .round))
@@ -105,7 +107,7 @@ struct WatchKickOffView: View {
 //        }
 //    }
 //
-//1초가 지날 때마다 -.33값이 되는 progress입니다.
+////1초가 지날 때마다 -.33값이 되는 progress입니다.
 //    func decreaseProgress() {
 //        if progress > 0.1 {
 //            if progress != 1 {
