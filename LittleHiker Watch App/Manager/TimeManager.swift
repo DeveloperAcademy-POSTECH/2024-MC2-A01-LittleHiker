e40381dc4b41673515aece4c24bc2e26707094c4
norang <norang1221@gmail.com>
2024-05-21 21:09:23 +0900

    Design: [feature/Time] displayTime 포맷 변경 00:00.00 -> 00:00:00

--- LittleHiker Watch App/Manager/TimeManager.swift
+++ LittleHiker Watch App/Manager/TimeManager.swift
@@ -11,7 +11,7 @@ class TimeManager: NSObject, ObservableObject {
     @Published var timer: Timer?
     @Published var elapsedTime: TimeInterval = 0
     @Published var isRunning = false
-    @Published var displayTime: String = "00:00.00"
+    @Published var displayTime: String = "00:00:00"
     
 //    var formattedElapsedTime: String {
 //        let minutes = Int(elapsedTime) / 60
@@ -61,7 +61,7 @@ class TimeManager: NSObject, ObservableObject {
         let minutes = Int(elapsedTime) / 60
         let seconds = Int(elapsedTime) % 60
         let milliseconds = Int((elapsedTime * 100).truncatingRemainder(dividingBy: 100))
-        self.displayTime = String(format: "%02d:%02d.%02d", minutes, seconds, milliseconds)
+        self.displayTime = String(format: "%02d:%02d:%02d", minutes, seconds, milliseconds)
         print("SetTime \(displayTime)")
     }
 }
