27894b65214cec6f4f979f23cd5e8bcf852b79d0
rokdam <travel_dob@naver.com>
2024-05-22 15:31:53 +0900

    Feat: [feature/Phone] SwiftData model 생성

--- LittleHiker/LittleHikerApp.swift
+++ LittleHiker/LittleHikerApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct LittleHikerApp: App {
+    var modelContainer: ModelContainer = {
+        let schema = Schema([HikingRecord.self, HikingLog.self])
+        // TODO: - CloudKit을 사용하게 되면 여기에서 cloudKitDatabase 옵션을 추가해줘야 함
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(modelContainer)
         }
     }
 }
