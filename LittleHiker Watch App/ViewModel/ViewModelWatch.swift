3bb1dd4d4aa8b2165adc16d76849d87b24cae4e0
rokdam <travel_dob@naver.com>
2024-05-22 10:58:53 +0900

    Feat: [feature/UI] watch에서 보정량 입력받으면 계산식 수정되도록 수정

--- LittleHiker Watch App/ViewModel/ViewModelWatch.swift
+++ LittleHiker Watch App/ViewModel/ViewModelWatch.swift
@@ -9,10 +9,9 @@ import Foundation
 import WatchConnectivity
 
 final class ViewModelWatch: NSObject, WCSessionDelegate, ObservableObject {
-    @Published var count: Int = 0
-
+    @Published var impulseRate: String = ""
     var session: WCSession
-
+    
     init(session: WCSession = .default) {
         self.session = session
         super.init()
@@ -21,12 +20,24 @@ final class ViewModelWatch: NSObject, WCSessionDelegate, ObservableObject {
     }
     
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-
+        
     }
     
+    // 다른 기기의 세션에서 sendMessage() 메서드로 메세지를 받았을 때 호출되는 메서드
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
         DispatchQueue.main.async {
-            self.count = message["count"] as? Int ?? 0
+            // 받은 메세지에서 원하는 Key값(여기서는 "message")으로 메세지 String을 가져온다.
+            // messageText는 Published 프로퍼티이기 때문에 DispatchQueue.main.async로 실행해줘야함
+            self.impulseRate = message["message"] as? String ?? "2.7"
         }
     }
+    
+    func handleImpulseRate(impulseRate: String) {
+        let impulseManager = ImpulseManager()
+        impulseManager.diagonalVelocityCriterion = impulseRate
+        // Now use viewModelWatch.impulseRate in your view or other logic
+    }
+    
+    
+    
 }
--- LittleHiker/View/ContentView.swift
+++ LittleHiker/View/ContentView.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var model = ViewModelIPhone()
+    @State var messsageText = "2.7"
     
     var body: some View {
         ScrollView {
             VStack {
                 Text("여기 밑에 Array가 뜰 것임 테스트용")
-                //TODO: - WatchOS에서 전달받은 데이터가 여기 바로 뜨면 됨!
+                Text("현재충격량:" + self.messsageText)
                 Text(self.model.message)
                     .font(.system(size: 8))
+                //충격량 입력받기
+                TextField("충격량을 입력해주세요", text: $messsageText)
+                Button {
+                    self.model.session.sendMessage(["message" : self.messsageText], replyHandler: nil) { error in
+                        /**
+                         다음의 상황에서 오류가 발생할 수 있음
+                         -> property-list 데이터 타입이 아닐 때
+                         -> watchOS가 reachable 상태가 아닌데 전송할 때
+                         */
+                        print(error.localizedDescription)
+                    }
+                } label: {
+                    Text("Send Message")
+                }.border(Color.black)
             }
             .padding()
         }
