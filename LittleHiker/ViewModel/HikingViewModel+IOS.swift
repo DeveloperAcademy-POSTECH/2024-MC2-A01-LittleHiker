0c7ed7b8e6506897601f6c803e7c3fe88bc1f120
Hyungeol94 <ekfrhkquf12@naver.com>
2024-12-02 12:30:30 +0900

    Feat: 변경된 모델 반영해 코드 수정

--- LittleHiker/ViewModel/HikingViewModel+IOS.swift
+++ LittleHiker/ViewModel/HikingViewModel+IOS.swift
@@ -22,7 +22,6 @@ class HikingViewModel: ObservableObject {
             // watch에서 받은 데이터를 HikingRecord에 저장
             var hikingRecords = dataSource.fetchHikingRecords()
             
-            
             Task{
                 if resultArray["data"] != nil { //summaryModel
                     await saveReceivedSummaryData(resultArray, hikingRecords, dataSource)
@@ -54,7 +53,7 @@ extension HikingViewModel {
                 where: {$0.id == UUID(uuidString: resultArray["id"] as? String ?? "")
                 }) {
                 hikingRecord.id = resultArray["id"] as! UUID
-                    
+                
                 // TODO: - 더 간단하게 쓸 수 있을지 리팩토링
                 if let title = data["title"] as? String {
                     hikingRecord.title = title
@@ -62,11 +61,13 @@ extension HikingViewModel {
                 if let duration = data["duration"] as? Int {
                     hikingRecord.duration = duration
                 }
-                if let startDate = data["startDate"] as? Date {
-                    hikingRecord.startDateTime = startDate
+                //healthKit에서 조회
+                if let startDateTime = data["startDate"] as? Date {
+                    hikingRecord.startDateTime = startDateTime
                 }
-                if let endDate = data["endDate"] as? Date {
-                    hikingRecord.endDateTime = endDate
+                //healthKit에서 조회
+                if let endDateTime = data["endDate"] as? Date {
+                    hikingRecord.endDateTime = endDateTime
                 }
                 if let minHeartRate = data["minHeartRate"] as? Int {
                     hikingRecord.minHeartRate = minHeartRate
@@ -77,54 +78,78 @@ extension HikingViewModel {
                 if let avgHeartRate = data["avgHeartRate"] as? Int {
                     hikingRecord.avgHeartRate = avgHeartRate
                 }
+                
+                // 헬스킷에서 조회
                 if let startAltitude = data["minAltitude"] as? Int {
                     hikingRecord.startAltitude = startAltitude
                 }
+                // watch에서 전송받음
                 if let peakAltitude = data["maxAltitude"] as? Int {
                     hikingRecord.peakAltitude = peakAltitude
                 }
+                // 헬스킷에서 조회
                 if let endAltitude = data["minAltitude"] as? Int {
                     hikingRecord.endAltitude = endAltitude
                 }
+                
+                // watch에서 전송받음
+                if let minAltitude = data["minAltitude"] as? Int {
+                    hikingRecord.minAltitude = minAltitude
+                }
+                
+                // watch에서 전송받음
+                if let maxAltitude = data["maxAltitude"] as? Int {
+                    hikingRecord.maxAltitude = maxAltitude
+                }
+                // 헬스킷에 없어서 watch에서 전송받아야 함 (현재 전송 로직 없음)
                 if let ascendAvgSpeed = data["ascendAvgSpeed"] as? Int {
                     hikingRecord.ascendAvgSpeed = ascendAvgSpeed
                 }
+                
+                // 헬스킷에 없어서 watch에서 전송받아야 함 (현재 전송 로직 없음)
                 if let descendAvgSpeed = data["descendAvgSpeed"] as? Int {
                     hikingRecord.descendAvgSpeed = descendAvgSpeed
                 }
+                
+                // watch에서 전송받음
                 if let avgSpeed = data["avgSpeed"] as? Double {
                     hikingRecord.avgSpeed = avgSpeed
                 }
+                
                 if let avgImpulse = data["avgImpulse"] as? Double {
                     hikingRecord.avgImpulse = avgImpulse
                 }
                 
-
+                //로그는 따로 관리
                 dataSource.saveItem(hikingRecord)
-                    
             } else {
                 let newHikingRecord = HikingRecord(
                     id: UUID(uuidString: resultArray["id"] as! String) ?? UUID(),
                     title: "\(data["startDate"] as? String ?? "-")",
                     duration: data["duration"] as? Int ?? 0,
-                    startDateTime: data["startDate"] as? Date ?? Date(),
-                    endDateTime: data["endDate"] as? Date ?? Date(),
+                    //optional type -> 헬스킷에서 조회
+                    //                    startDateTime: data["startDate"] as? Date ?? Date(),
+                    //                    endDateTime: data["endDate"] as? Date ?? Date(),
+                    peakDateTime: data["peakDate"] as? Date? ?? nil,
                     minHeartRate: data["minHeartRate"] as? Int ?? 0,
                     maxHeartRate: data["maxHeartRate"] as? Int ?? 0,
                     avgHeartRate: data["heartRateAvg"] as? Int ?? 0,
-                    startAltitude: data["minAltitude"] as? Int ?? 0,
-                    peakAltitude: data["maxAltitude"] as? Int ?? 0,
-                    endAltitude: data["minAltitude"] as? Int ?? 0,
+                    //optional type
+                    //                    startAltitude: data["minAltitude"] as? Int ?? 0,
+                    //                    endAltitude: data["minAltitude"] as? Int ?? 0,
+                    peakAltitude: data["maxAltitude"] as? Int? ?? nil,
+                    minAltitude: data["minAltitude"] as? Int ?? 0,
+                    maxAltitude: data["maxAltitude"] as? Int ?? 0,
                     ascendAvgSpeed: data["ascendAvgSpeed"] as? Int ?? 0,
                     descendAvgSpeed: data["descendAvgSpeed"] as? Int ?? 0,
                     avgSpeed: data["avgSpeed"] as? Double ?? 0.0,
                     avgImpulse: data["avgImpulse"] as? Double ?? 0.0,
-                    hikingLog: [:])
+                    hikingLog: [])
                 dataSource.saveItem(newHikingRecord)
             }
         }
     }
-        
+    
     @MainActor
     func saveReceivedLogs  (
         _ resultArray: [String: Any],
@@ -135,12 +160,12 @@ extension HikingViewModel {
             if let hikingRecord = hikingRecords.first(
                 where: {$0.id == UUID(uuidString: resultArray["id"] as? String ?? "")
                 }) {
-                hikingRecord.hikingLog = logs["logs"] as? [String: String] ?? [:]
+                hikingRecord.hikingLog = logs["logs"] as? [HikingLog] ?? []
                 dataSource.saveItem(hikingRecord)
             } else {
                 let newHikingRecord =
                 HikingRecord(
-                    id: UUID(uuidString: resultArray["id"] as! String) ?? UUID(),
+                    id: UUID(uuidString: resultArray["id"] as? String ?? UUID().uuidString) ?? UUID(),
                     title: "-",
                     duration: 0,
                     startDateTime: Date(),
@@ -148,20 +173,32 @@ extension HikingViewModel {
                     minHeartRate: 0,
                     maxHeartRate: 0,
                     avgHeartRate: 0,
-                    startAltitude: 0,
-                    peakAltitude: 0,
-                    endAltitude: 0,
+                    minAltitude: 0,
+                    maxAltitude: 0,
                     ascendAvgSpeed: 0,
                     descendAvgSpeed: 0,
                     avgSpeed: 0.0,
                     avgImpulse: 0.0,
-                    hikingLog: logs["logs"] as? [String: String] ?? [:]
+                    hikingLog: convertLogsToHikingLogs(from: logs["logs"] as? [String:String] ?? [:])
                 )
                 dataSource.saveItem(newHikingRecord)
-                    
+                
             }
         }
     }
-        
-        
+    
+    
+    func convertLogsToHikingLogs(from logs: [String:String]) -> [HikingLog] {
+        var convertedLogs: [HikingLog] = []
+        for (timeStampString, impulseLog) in logs {
+            convertedLogs.append(
+                HikingLog(
+                    id: UUID(),
+                    impulse: Int(impulseLog) ?? 0 ,
+                    timeStamp: Date(timeIntervalSince1970: Double(timeStampString)!)
+                )
+            )
+        }
+        return convertedLogs
+    }
 }
