14c03d72f20359903e0e09ef87edccd65aef7620
Bell-isHere <quf417@gmail.com>
2025-01-15 01:47:03 -0500

    Chore: 코드리뷰 반영

--- LittleHiker Watch App/View/WatchKickOffView.swift
+++ LittleHiker Watch App/View/WatchKickOffView.swift
@@ -18,39 +18,30 @@ struct WatchKickOffView: View {
             if showCountdown {
                 WatchCountDownView(viewModel: viewModel, timeManager: timeManager)
             } else {
-                KickOffView {
-                    showCountdown = true
+                VStack {
+                    Button(action: {
+                        showCountdown = true
+                    }) {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.main)
+                            .overlay {
+                                Text("시작")
+                                    .font(.system(size: 32))
+                                    .fontWeight(.semibold)
+                                    .foregroundStyle(.black)
+                            }
+                            .shadow(color: .main.opacity(0.3), radius: 12)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .padding()
+                    
+                    Text("리틀하이커랑 등산하기")
+                        .font(.system(size: 14))
+                        .fontWeight(.medium)
+                        .padding(.top, 6)
                 }
             }
-        }
-    }
-}
-
-// 시작 버튼 화면
-struct KickOffView: View {
-    let onStart: () -> Void
-    
-    var body: some View {
-        VStack {
-            Button(action: onStart) {
-                Circle()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.main)
-                    .overlay {
-                        Text("시작")
-                            .font(.system(size: 32))
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.black)
-                    }
-                    .shadow(color: .main.opacity(0.3), radius: 12)
-            }
-            .buttonStyle(PlainButtonStyle())
-            .padding()
-            
-            Text("리틀하이커랑 등산하기")
-                .font(.system(size: 14))
-                .fontWeight(.medium)
-                .padding(.top, 6)
         }
     }
 }
