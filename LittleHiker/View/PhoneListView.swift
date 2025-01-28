86de5ef3b1fdb4028e07d564ba02bfed95e5c221
Hyungeol94 <55428818+Hyungeol94@users.noreply.github.com>
2025-01-28 17:16:01 +0900

    데이터 없을 때 화면 추가 (#135)
    * Chore: add Assets
    
    * Feat: 산행 데이터 없을 때 화면 추가
    
    * Design: [feature/NoDataView] 디자인 반영하여 패딩값 수정
    
    * Refactor: [feature/NoDataView] NoDataView 분리
    
    * Design: [feature/NoDataView]
    - 타이틀에 회색  백그라운드 컬러 추가
    - 검색 뷰 주석처리
    
    * Feat: [feature/NoDataView] 산행기록 순서를 오래된 순으로 정렬

--- LittleHiker/View/PhoneListView.swift
+++ LittleHiker/View/PhoneListView.swift
@@ -17,45 +17,68 @@ struct PhoneListView: View {
     //추후 검색용 텍스트
     @State var text: String = ""
     
+    var sortedRecords: [HikingRecord] {
+        records.sorted { $0.startDateTime ?? Date() < $1.startDateTime ?? Date() }
+    }
+    
     var body: some View {
         NavigationStack{
-            VStack {
-                HStack {
-                    Text("산행기록")
-                        .font(.system(size: 34, weight: .bold))
-                    Spacer()
+            ZStack(alignment: .bottom){
+                if records.count == 0 { //데이터가 없을 때 보여질 뷰
+                    NoDataView()
                 }
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundColor(.gray)
-                        .padding(.leading, 7)
-                    TextField("Search", text: $text)
-                        .padding(7)
-                        .cornerRadius(8)
-                }
-                .background(Color(.systemGray5))
-                .cornerRadius(10)
-            }
-            
-            List {
-                ForEach(records) { record in
-                    NavigationLink(destination: PhoneDetailView(record: record)) {
-                        PhoneRowView(record: record)
-                            .swipeActions(edge: .trailing) {   
-                                Button(action: {
-                                    deleteRecord(record: record)
-                                }) {
-                                    Label("Delete", systemImage: "trash")
-                                }
-                                .tint(.red)
+                VStack{
+                    VStack {
+                        ZStack {
+                            HStack {
+                                Text("산행기록")
+                                    .font(.system(size: 34, weight: .bold))
+                                Spacer()
                             }
+                            .padding(.top, 44)
+                            .padding(.leading, 16)
+                            .padding(.trailing, 16)
+                            .padding(.bottom, 8)
+                        }
+                        .background(Color(.systemGray5))
+                        //검색 뷰 삭제
+//                        HStack {
+//                            Image(systemName: "magnifyingglass")
+//                                .foregroundColor(.gray)
+//                                .padding(.leading, 7)
+//                            TextField("Search", text: $text)
+//                                .padding(7)
+//                                .cornerRadius(8)
+//                        }
+//                        .background(Color(.systemGray5))
+//                        .cornerRadius(10)
+//                        .padding(.leading, 16)
+//                        .padding(.trailing, 16)
                     }
+                    
+                    
+                    List {
+                        ForEach(sortedRecords) { record in
+                            NavigationLink(destination: PhoneDetailView(record: record)) {
+                                PhoneRowView(record: record)
+                                    .swipeActions(edge: .trailing) {
+                                        Button(action: {
+                                            deleteRecord(record: record)
+                                        }) {
+                                            Label("Delete", systemImage: "trash")
+                                        }
+                                        .tint(.red)
+                                    }
+                            }
+                        }
+                    }
+                    .listStyle(.plain)
                 }
             }
-            .listStyle(.plain)
-            
+//            .edgesIgnoringSafeArea(.bottom)
         }
     }
+    
     
     private func deleteRecord(record: HikingRecord) {
         withAnimation {
@@ -64,4 +87,25 @@ struct PhoneListView: View {
         // TODO: healthKit 기록도 지울 것인지 action sheet로 확인 필요
     }
     
+}
+
+private struct NoDataView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Image("NoDataSquirrel")
+                .resizable()
+                .frame(width: 128, height: 128)
+            Text("아직 산행 기록이 없네요!\n지금부터 첫 산행을 기록해 보세요")
+                .font(.custom("Moneygraphy-Rounded", size: 16))
+                .fontWeight(.regular)
+                .multilineTextAlignment(.center)
+                .padding(.top, 16)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    PhoneListView()
 }
